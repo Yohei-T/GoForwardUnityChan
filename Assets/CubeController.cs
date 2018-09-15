423b59ca9499c31b95ae84cff24ddb6ef36428f0
Yohei-T <yohei.tsukuda@outlook.jp>
2018-09-16 02:08:46 +0900

    GoForwardUnityChan 1回目のコミット
    課題完了

---
+++ Assets/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    public AudioClip block;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "unitychan")
+        {
+            GetComponent<AudioSource>().PlayOneShot(block, 1);
+        }
+        
+    }
+}
